d1e031ec2356a149edbd9216172a8181bfd7226e
Guilherme Silva <guimoruntri@gmail.com>
2022-04-12 18:56:31 -0300

    Projeto Criado

---
+++ src/main/java/com/tools/java/challenge/ToolsJavaChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.tools.java.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolsJavaChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToolsJavaChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tools/java/challenge/ToolsJavaChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tools.java.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolsJavaChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
